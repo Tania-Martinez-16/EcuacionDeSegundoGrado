546cfc873a43fa9b3001f685838c212e9acd5f43
Tania Georgina Martinez Tuz <16470257@itcampeche.edu.mx>
2017-05-24 03:04:56 -0500

    Version final del proyecto

--- src/ecuaciondesegundogrado/Main.java
+++ src/ecuaciondesegundogrado/Main.java
@@ -5,6 +5,9 @@
  */
 package ecuaciondesegundogrado;
 
+import java.util.Scanner;
+import modelo.EcuacionDeSegundoGrado;
+
 /**
  *
  * @author tatia
@@ -15,7 +18,41 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Scanner entrada = new Scanner (System.in);
     
+    // 1er Caso
+        // Objeto
+        EcuacionDeSegundoGrado miEcuacionNo1 = new EcuacionDeSegundoGrado(8, 6, -15);
+
+        // Cálculo
+        System.out.println(" Valor del discriminante:  " + miEcuacionNo1.getDiscriminante());
+        System.out.println(" Número de Raices:  " + miEcuacionNo1.getNumeroDeSolucionesReales());
+        System.out.println(" x1= " + miEcuacionNo1.getRaiz1());
+        System.out.println(" x2= " + miEcuacionNo1.getRaiz2());
+        
+    System.out.println("____________________________________________________________________");    
+    
+        // 2do Caso
+        // Objeto
+        EcuacionDeSegundoGrado miEcuacionNo2 = new EcuacionDeSegundoGrado(10, 5, 0);
+
+        // Cálculo
+        System.out.println(" Valor del discriminante:  " + miEcuacionNo2.getDiscriminante());
+        System.out.println(" Número de Raices:  " + miEcuacionNo2.getNumeroDeSolucionesReales());
+        System.out.println(" x1= " + miEcuacionNo2.getRaiz1());
+        System.out.println(" x2= " + miEcuacionNo2.getRaiz2());
+        
+    System.out.println("____________________________________________________________________");
+   
+        // 3er Caso
+        // Objeto
+        EcuacionDeSegundoGrado miEcuacionNo3 = new EcuacionDeSegundoGrado(-20, -7, 2);
+
+        // Cálculo
+        System.out.println(" Valor del discriminante:  " + miEcuacionNo3.getDiscriminante());
+        System.out.println(" Número de Raices:  " + miEcuacionNo3.getNumeroDeSolucionesReales());
+        System.out.println(" x1= " + miEcuacionNo3.getRaiz1());
+        System.out.println(" x2= " + miEcuacionNo3.getRaiz2());
+        
+    } 
 }
--- src/modelo/EcuacionDeSegundoGrado.java
+++ src/modelo/EcuacionDeSegundoGrado.java
@@ -10,5 +10,67 @@ package modelo;
  * @author tatia
  */
 public class EcuacionDeSegundoGrado {
+    // Variables de instancia
+    public double a;
+    public double b;
+    public double c;
+    public int numeroDeRaices;
+    public double raiz1;
+    public double raiz2;
     
-}
+    // Constructor
+    public EcuacionDeSegundoGrado (double a, double b, double c){
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    }
+    
+    // getCoeficienteA
+    public double getCoeficienteA(){
+        return a;
+    }
+    
+    // getCoeficienteB
+    public double getCoeficienteB(){
+        return b;
+    }
+    
+    // getCoeficienteC
+    public double getCoeficienteC(){
+        return c;
+    }
+    
+    // getDiscriminante
+    public double getDiscriminante(){
+        double discriminante = (b*b) - 4 * a * c;
+        return discriminante;
+    }
+    
+    //  getNumeroDeSoluciones
+    public double getNumeroDeSolucionesReales(){
+        double dis = getDiscriminante();
+        if (dis > 0){
+            numeroDeRaices = 2;
+        }
+        else 
+            if (dis < 0){
+            numeroDeRaices = 0;
+        }
+            else {
+                numeroDeRaices = 1;
+            }
+        return numeroDeRaices;
+    }
+    
+    // getRaiz1
+    public double getRaiz1(){
+        raiz1 = (-b + Math.sqrt(getDiscriminante())) / (2 * a);
+        return raiz1;
+    }
+    
+    // getRaiz2
+    public double getRaiz2(){
+        raiz2 = (-b - Math.sqrt(getDiscriminante())) / (2 * a);
+        return raiz2;
+    }
+}    
